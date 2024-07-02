3d9e7b91dc0e7b9657b4384a02cbfc4b55d1b3d4
WheresMySauce <thoai4067@gmail.com>
2024-07-02 18:22:15 +0700

    up

--- myapp.py
+++ myapp.py
@@ -128,4 +128,4 @@ def weld_check():
     else:
         return jsonify({'weld_check': "Weld bead detected"})
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000) 
+    app.run(debug=True, host="0.0.0.0", port=8000) 
